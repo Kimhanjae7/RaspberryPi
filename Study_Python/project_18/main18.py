a3862bcccfd2603d5988cc7e801bb4b878430ada
Kimhanjae7 <hjk2512@naver.com>
2025-01-27 16:42:15 +0900

    LED blink with libgpiod

--- Study_Python/project_18/main18.py
+++ Study_Python/project_18/main18.py
@@ -6,7 +6,7 @@ try:
         now = datetime.datetime.now()  # 현재 날짜와 시간 가져오기
         nowSec = now.second  # 현재 시간의 초 단위 가져오기
 
-        # 현재 초가 0일 때 (즉, 1분마다다)
+        # 현재 초가 0일 때 (즉, 1분마다)
         if nowSec == 0:
             # 현재 시간을 지정된 형식으로 문자열로 변환
             nowTime = now.strftime("%H시 %M분 %S초 입니다.")
